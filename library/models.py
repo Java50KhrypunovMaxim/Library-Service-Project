c82e537397fd68437e9fa59c1e8fa4e35829a58c
Maxim Khrypunov <khrypunovmax@gmail.com>
2025-04-10 16:51:11 +0300

    add models.py

--- library/models.py
+++ library/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+
+    class CoverType(models.TextChoices):
+        HARD = 'HARD', 'Hard Cover'
+        SOFT = 'SOFT', 'Soft Cover'
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(max_length=4, choices=CoverType.choices)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.title
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.last_name} borrowed {self.book.title}"
+
+
+class Payment(models.Model):
+    class Status(models.TextChoices):
+        PENDING = 'PENDING', 'Pending'
+        PAID = 'PAID', 'Paid'
+
+    class Type(models.TextChoices):
+        PAYMENT = 'PAYMENT', 'Payment'
+        FINE = 'FINE', 'Fine'
+
+    status = models.CharField(max_length=7, choices=Status.choices, default=Status.PENDING)
+    type = models.CharField(max_length=7, choices=Type.choices)
+    borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE)
+    session_url = models.URLField(max_length=500)
+    session_id = models.CharField(max_length=255)
+    money_to_pay = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.type} - {self.money_to_pay}$ ({self.status})"
