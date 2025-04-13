f3d5267563aa98719bafa37136525d0c6645d5d4
Maxim Khrypunov <khrypunovmax@gmail.com>
2025-04-13 14:34:45 +0300

    add user models.py serializers.py urls views.py

--- Library_project/settings.py
+++ Library_project/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'Library_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DDATABASES = {
+DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
--- library/models.py
+++ library/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 
+from user.models import User
+
+
 class Book(models.Model):
 
     class CoverType(models.TextChoices):
-        HARD = 'HARD', 'Hard Cover'
-        SOFT = 'SOFT', 'Soft Cover'
+        HARD = 'Hard Cover'
+        SOFT = 'Soft Cover'
 
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
-    cover = models.CharField(max_length=4, choices=CoverType.choices)
+    cover = models.CharField(max_length=20, choices=CoverType.choices)
     inventory = models.PositiveIntegerField()
     daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
 
@@ -21,20 +24,20 @@ class Borrowing(models.Model):
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.user.last_name} borrowed {self.book.title}"
+        return f"{self.user.email} borrowed {self.book.title}"
 
 
 class Payment(models.Model):
     class Status(models.TextChoices):
-        PENDING = 'PENDING', 'Pending'
-        PAID = 'PAID', 'Paid'
+        PENDING = 'PENDING'
+        PAID = 'PAID'
 
     class Type(models.TextChoices):
-        PAYMENT = 'PAYMENT', 'Payment'
-        FINE = 'FINE', 'Fine'
+        PAYMENT = 'PAYMENT'
+        FINE = 'FINE'
 
     status = models.CharField(max_length=7, choices=Status.choices, default=Status.PENDING)
     type = models.CharField(max_length=7, choices=Type.choices)
