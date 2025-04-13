5c883834f2ff1752345e626a5f17b4f9b80dea26
Maxim Khrypunov <khrypunovmax@gmail.com>
2025-04-13 15:00:19 +0300

    add urls

---
+++ library/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from library.views import BookViewSet, PaymentViewSet, BorrowingViewSet
+
+router = routers.DefaultRouter()
+router.register("books", BookViewSet)
+router.register("payments", PaymentViewSet)
+router.register("borrowings", BorrowingViewSet)
+
+urlpatterns = [path("", include(router.urls))]
+
+app_name = "cinema"
